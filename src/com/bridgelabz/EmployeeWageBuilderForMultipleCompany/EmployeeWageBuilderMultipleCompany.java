a2d0910016f1b2b89af93771a2b2a36b650a170c
HarishKamaras <harishkamarasu@gmail.com>
2022-05-04 17:03:27 +0530

    EmployeeWageBuilderMultipleCompany

---
+++ src/com/bridgelabz/EmployeeWageBuilderForMultipleCompany/EmployeeWageBuilderMultipleCompany.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmployeeWageBuilderForMultipleCompany;
+
+public class EmployeeWageBuilderMultipleCompany {
+    public static void main(String[] args) {
+        System.out.println("EmployeeWageBuilderMultipleCompany");
+    }
+}
+
