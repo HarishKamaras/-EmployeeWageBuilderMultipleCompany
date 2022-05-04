9840bf09bf5446efe82dbfcf406547f9cfa0b347
HarishKamaras <harishkamarasu@gmail.com>
2022-05-04 17:06:09 +0530

    CalculateDailyWage

--- src/com/bridgelabz/EmployeeWageBuilderForMultipleCompany/EmployeeWageBuilderMultipleCompany.java
+++ src/com/bridgelabz/EmployeeWageBuilderForMultipleCompany/EmployeeWageBuilderMultipleCompany.java
@@ -1,11 +1,19 @@
 package com.bridgelabz.EmployeeWageBuilderForMultipleCompany;
 
 public class EmployeeWageBuilderMultipleCompany {
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOUR = 10;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to EmployeeWageBuilderMultipleCompany");
+
+        System.out.println("Welcome to Employee wage Computation Problem");
+        int dailyWage;
         int empStatus = (int) (Math.random() * 10) % 2;
         if(empStatus==1) {
             System.out.println("Employee present");
+            //UC2 calculate Daily wage
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("daily wage : "+dailyWage);
         }
         else{
             System.out.println("Employee Absent");
